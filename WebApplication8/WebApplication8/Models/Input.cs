22b8b18bc92d49a7a3b4211e6ad406aea9f2a7a3
Rijuta wagh <rijutawagh04@gmail.com>
2017-04-29 00:10:21 -0400

    Web application

---
+++ WebApplication8/WebApplication8/Models/Input.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8.Models
+{
+    public class Input
+    {
+        public string data { get; set; }
+        public string productID { get; set; }
+        public string SL { get; set; }
+        public string SP { get; set; }
+
+
+    }
+}
